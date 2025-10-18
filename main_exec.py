d2a98f4f9d88fdb6db79b0c7a80199602b101775
namkha1032 <namkha1032@gmail.com>
2025-10-18 20:27:10 +1030

    update test program

--- main_exec.py
+++ main_exec.py
@@ -1,4 +1,7 @@
 from main_func import exec_program
-
+from logab import log_wrap
 if __name__ == "__main__":
-    exec_program()
+    with log_wrap(
+        log_file="./app.log",
+        log_level="debug", print_level="debug"):
+        exec_program()
--- main_func.py
+++ main_func.py
@@ -15,29 +15,26 @@ def print_main():
     logger = log_init()
     logger.warning("In function of main.py")
 def exec_program():
-    with log_wrap(
-        # log_file="./app.log",
-        log_level="debug", print_level="debug"):
-        logger = log_init()
-        for idx, char in enumerate(range(100)):
-            # supermegaprint(idx)
-            time.sleep(1)
-            logger.info(f"idx: {idx}")
-            if idx == 1:
-                print_main()
-            if idx == 2:
-                supermegaprint()
-                logger.warning(f"Some warning message")
-            if idx == 3:
-                critical_function()
-            if idx == 4:
-                override_print()
-            if idx == 5:
-                a_very_long_function()
-            if idx == 6:
-                newfunc()
-            if idx == 7:
-                error_func()
+    logger = log_init()
+    for idx, char in enumerate(range(100)):
+        # supermegaprint(idx)
+        time.sleep(1)
+        logger.info(f"idx: {idx}")
+        if idx == 1:
+            print_main()
+        if idx == 2:
+            supermegaprint()
+            logger.warning(f"Some warning message")
+        if idx == 3:
+            critical_function()
+        if idx == 4:
+            override_print()
+        if idx == 5:
+            a_very_long_function()
+        if idx == 6:
+            newfunc()
+        if idx == 7:
+            error_func()
 
 
 
